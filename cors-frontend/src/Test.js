34c5e1d6b74bb12214b244858fee278999367ed9
Felix <felix.waldbach@outlook.com>
2019-07-19 17:26:35 -0700

    Removed submodule

---
+++ cors-frontend/src/Test.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import $ from "jquery";
+
+export default class Test extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            test: 'not tested'
+        };
+    }
+
+    executeTestRequest(e) {
+        e.preventDefault();
+        let app = this;
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                    url: "http://localhost:3000/test",
+                    cache: false,
+                    type: "GET",
+                    contentType: 'application/json',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    success: function (data) {
+                        app.setState({
+                            test: data
+                        })
+                    },
+                    error: function (xhr, status, err) {
+                        reject(err);
+                    }
+                }
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.test}
+                <button onClick={this.executeTestRequest.bind(this)}>Hit test</button>
+            </div>
+        )
+    }
+}
