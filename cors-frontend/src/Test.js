c2ffad32c2896790868d450b7ca4b4fc751c45c3
Felix Waldbach <felix.waldbach@hpe.com>
2019-07-24 11:56:55 -0700

    Executing get method in constructor

--- cors-frontend/src/Test.js
+++ cors-frontend/src/Test.js
@@ -9,6 +9,28 @@ export default class Test extends Component {
         this.state = {
             test: 'not tested'
         };
+
+        $.ajax({
+                url: process.env.REACT_APP_BACKEND_ADDRESS,
+                cache: false,
+                type: "GET",
+                contentType: 'application/json',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer eohfoedsfdsoinfdsonfandsf'
+                },
+                data: {hello: 'world'},
+                success: function (data) {
+                    app.setState({
+                        test: data
+                    })
+                },
+                error: function (xhr, status, err) {
+                    reject(err);
+                }
+            }
+        );
     }
 
     executeTestRequest(e) {
