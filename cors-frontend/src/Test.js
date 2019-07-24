c06980218ab4f683da3151288c79fc7cdad816be
Felix Waldbach <felix.waldbach@hpe.com>
2019-07-24 11:45:16 -0700

    Added test route back

--- cors-frontend/src/Test.js
+++ cors-frontend/src/Test.js
@@ -11,13 +11,32 @@ export default class Test extends Component {
         };
     }
 
-    async executeTestRequest(e) {
+    executeTestRequest(e) {
         e.preventDefault();
-        let response = await testRoute();
-        console.log(response);
-        this.setState({
-            test: response
-        })
+        let app = this;
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                    url: process.env.REACT_APP_BACKEND_ADDRESS,
+                    cache: false,
+                    type: "GET",
+                    contentType: 'application/json',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer eohfoedsfdsoinfdsonfandsf'
+                    },
+                    data: {hello: 'world'},
+                    success: function (data) {
+                        app.setState({
+                            test: data
+                        })
+                    },
+                    error: function (xhr, status, err) {
+                        reject(err);
+                    }
+                }
+            );
+        });
     }
 
     render() {
